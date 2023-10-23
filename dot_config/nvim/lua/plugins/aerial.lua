8936f88bc3787f1985f45f4c760e1586820c0b69
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2023-10-23 08:21:08 +0530

    add folding to aerial

--- dot_config/nvim/lua/plugins/aerial.lua
+++ dot_config/nvim/lua/plugins/aerial.lua
@@ -4,5 +4,9 @@ return {
     opts.layout.min_width = 40
     -- opts.filter_kind = false
     opts.open_automatic = true
+    opts.manage_folds = true
+    opts.link_folds_to_tree = true
+    opts.link_tree_to_folds = true
+    opts.show_guides = true
   end,
 }
