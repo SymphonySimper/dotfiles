b1d40bfd0b068cbf71abd92fa99bfad537b135ca
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2023-10-23 16:24:52 +0530

    tailwind sorter file types

--- dot_config/nvim/lua/plugins/tailwind-sorter.lua
+++ dot_config/nvim/lua/plugins/tailwind-sorter.lua
@@ -4,5 +4,19 @@ return {
   build = "cd formatter && npm i && npm run build",
   config = {
     on_save_enabled = true,
+    on_save_pattern = {
+      "*.html",
+      "*.js",
+      "*.jsx",
+      "*.tsx",
+      "*.twig",
+      "*.hbs",
+      "*.php",
+      "*.heex",
+      "*.astro",
+      "*.vue",
+      "*.svelte",
+    },
+    node_path = "node",
   },
 }
