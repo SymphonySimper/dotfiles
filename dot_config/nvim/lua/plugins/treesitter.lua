0dc340035690fc7a9166f52dfd03ba5a511547e6
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2023-10-23 07:34:14 +0530

    fix treesitter override config

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,13 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
+  opts = function(_, opts)
+    vim.list_extend(opts.ensure_installed, {
       "bash",
       "c",
+      "css",
+      "devicetree",
+      "gitcommit",
+      "gitignore",
       "html",
       "javascript",
       "jsdoc",
@@ -16,14 +20,14 @@ return {
       "python",
       "query",
       "regex",
+      "svelte",
       "tsx",
       "typescript",
       "vim",
       "vimdoc",
-      "yaml",
-      "svelte",
       "vue",
-    },
-    ignore_install = { "help" },
-  },
+      "yaml",
+    })
+    -- opts.ignore_install = { "help" }
+  end,
 }
