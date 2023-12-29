25e4f208d70cebdab9cbf42589da1cf19fed9c7e
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2023-12-29 06:53:52 +0530

    disable aerial on startup

--- dot_config/nvim/lua/plugins/aerial.lua
+++ dot_config/nvim/lua/plugins/aerial.lua
@@ -3,7 +3,7 @@ return {
   opts = function(_, opts)
     opts.layout.min_width = 40
     opts.filter_kind = false
-    opts.open_automatic = true
+    opts.open_automatic = false
     opts.manage_folds = true
     opts.link_folds_to_tree = true
     opts.link_tree_to_folds = true
