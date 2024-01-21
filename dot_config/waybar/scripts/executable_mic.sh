ffc34daa26673b2632e303c03279a56ddab7d98b
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-01-21 19:12:03 +0530

    rm notify-send from volume

--- dot_config/waybar/scripts/executable_mic.sh
+++ dot_config/waybar/scripts/executable_mic.sh
@@ -4,13 +4,13 @@ on=""
 off=""
 
 if [[ $(volume -gM) -eq 0 ]]; then
-	text="$on"
-	class="on"
-	tooltip="Everyone can hear you."
-else
 	text="$off"
 	class="off"
 	tooltip="No one can hear you."
+else
+	text="$on"
+	class="on"
+	tooltip="Everyone can hear you."
 fi
 
 printf '{"text":"%s","class":"%s","tooltip":"%s"}\n' "$text" "$class" "$tooltip"
