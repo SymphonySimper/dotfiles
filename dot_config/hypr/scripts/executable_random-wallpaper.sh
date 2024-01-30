a2fdb90bf467b762facde3fb07233f48a8c62f39
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-01-30 20:47:59 +0530

    create hyprpaper conf on the run

---
+++ dot_config/hypr/scripts/executable_random-wallpaper.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+dir="$HOME/.local/share/my/pics/wallpapers/"
+
+function create_config() {
+	printf 'ipc = off\npreload = %s\nwallpaper = ,%s\n' "$1" "$1" >"$HOME/.config/hypr/hyprpaper.conf"
+}
+
+if [[ -d $dir ]]; then
+	wallpaper="$(find "$dir" -type f | sort -R | head -n1)"
+	create_config "$wallpaper"
+else
+	create_config "$HOME/.local/share/my/pics/bg.png"
+fi
+hyprpaper
