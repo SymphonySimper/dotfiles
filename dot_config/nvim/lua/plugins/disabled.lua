a4271db8ca6f017fb6597c212296cda307993602
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-02-23 10:01:05 +0530

    feat: enable flash.nvim

--- dot_config/nvim/lua/plugins/disabled.lua
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -1,9 +1,4 @@
 return {
-  { "ggandor/leap.nvim", enabled = false },
-  -- flit depends on leap
-  { "ggandor/flit.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
-  { "folke/flash.nvim", enabled = false },
-  -- { "folke/noice.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
 }
