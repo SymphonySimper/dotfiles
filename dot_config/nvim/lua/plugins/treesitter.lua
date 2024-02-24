66a2a9b7f9909ed04947542a3120f416f02ec852
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-02-25 03:32:34 +0530

    Revert "feat: rm vim-matchup and vim-matchquote"
    This reverts commit ff8ff97049c24b825834c910fd6878ca0b16780d.

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -31,5 +31,10 @@ return {
       "yaml",
     })
     -- opts.ignore_install = { "help" }
+    -- See: https://github.com/andymass/vim-matchup
+    opts.matchup = {
+      enable = true,
+      include_match_words = true,
+    }
   end,
 }
